70ec5a392fb3c3a8b8fe7b11e4e16befc7736c2c
Ali Hassan <ahassan@inthekloud.com>
2021-08-04 15:21:23 +0200

    Working With Data and Models

--- Fructus/App/OnboardingView.swift
+++ Fructus/App/OnboardingView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK - PROPERTIES
-    
+    var fruits: [Fruit] = fruitsData
     
     // MARK - BODY
     var body: some View {
         TabView{
-            ForEach(0..<5){  item in 
-                FruitCardView()
+           
+            ForEach(fruits){ item in
+                FruitCardView(fruit: item)
             }
             
         }
@@ -26,6 +27,6 @@ struct OnboardingView: View {
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(fruits: fruitsData)
     }
 }
